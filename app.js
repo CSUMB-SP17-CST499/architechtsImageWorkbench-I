dbb6c7b2819b0d70a9b554b54d1a20bea4e9f0ec
Benito Sanchez <sanchezbenitog@gmail.com>
2017-02-09 13:18:10 -0800

    Default setup

---
+++ app.js
@@ -0,0 +1,9 @@
+class Main extends React.Component {
+    render() {
+        return (
+            <li>Hello world </li>
+        )
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById('app'));
