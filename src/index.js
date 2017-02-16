cc8b52e407643b9bd4aae21b7853bd903cf36651
Austin Zielinski <azielinski@csumb.edu>
2017-02-15 20:38:06 -0800

    implemented basic image uploader; will refine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import ImageUpload from './ImageUpload';
+
 import './index.css';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
+);
+
+ReactDOM.render(
+  <ImageUpload />,
+  document.getElementById('image')
 );
