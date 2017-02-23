0c3b867308ab4f9e2ae1153caa09fd8966271135
Austin Zielinski <azielinski@csumb.edu>
2017-02-22 21:47:51 -0800

    changed React logo to Panafold logo. Changed some formatting. Edited ImageUpload code to make it more concise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../img/pan-logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-        <p  id="headerid" className="App-header">
-            <h2>Designers&#39; ImagEngine</h2>
-        </p>
-
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Designers&apos; ImagEngine</h2>
+        </div>
+      </div>
     );
   }
 }
