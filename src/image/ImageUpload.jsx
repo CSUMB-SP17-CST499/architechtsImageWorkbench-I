726307ddafeadea62f9e8f85615889392a121752
araceli19 <agopar@csumb.edu>
2017-03-31 20:17:39 -0700

    Added image upload to it's own file and added rekognition to tag uploaded image
    Summary: Added ImageUploadService.js to facilite image upload to later add more function that the uploaded image might need. Added TagImage.js so that uploaded images are immediately tagged by rekognition as they are uploaded.
    
    Test Plan: npm start, open "inspect element" so that the tags of the image display via the console. Also check that an image uploads to s3 bucket.
    
    Reviewers: benFallas, austinz, gabez
    
    Reviewed By: benFallas
    
    Differential Revision: https://imagengine.phacility.com/D18

--- src/image/ImageUpload.jsx
+++ src/image/ImageUpload.jsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD:src/image/ImageUpload.js
-=======
 /* global FileReader */
 import AWS from 'aws-sdk';
->>>>>>> pushing config changes and changed code due to style guide. Nodemon is WIP:src/image/ImageUpload.jsx
 import React, { Component } from 'react';
 
 import S3Gallery from './imageSupport/S3Gallery';
@@ -40,46 +37,7 @@ class ImageUpload extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-<<<<<<< HEAD:src/image/ImageUpload.js
-    var fileBody = this.state.file;
-    ImageUploadService.upload(fileBody); //send to s3 bucket
-
-=======
-    let access = process.env.AWS_ACCESS_KEY_ID;
-    let secret = process.env.AWS_SECRET_ACCESS_KEY;
-    let region = 'us-west-2';
-
-    if (access == null || secret == null) {
-      access = localCredentials.accessKeyId;
-      secret = localCredentials.secretAccessKey;
-      region = localCredentials.region;
-    }
-
-    const credentials = new AWS.Credentials(access, secret);
-
-    AWS.config.update({
-      credentials,
-      region,
-    });
-
-    const s3image = new AWS.S3();
-
-    const params = {
-      Key: this.state.file.name,
-      Body: this.state.file,
-      Bucket: 'testing-uswest2',
-      ACL: 'public-read',
-    };
-
-    const options = {
-      partSize: 10 * 1024 * 1024,
-      queueSize: 1,
-    };
-
-    s3image.upload(params, options, (err, data) => {
-      console.log(err, data);
-    });
->>>>>>> pushing config changes and changed code due to style guide. Nodemon is WIP:src/image/ImageUpload.jsx
+    ImageUploadService.upload(this.state.file); // send to s3 bucket
   }
 
   render() {
