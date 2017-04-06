196f4e7a9e05038a91b4e40f0901d66f7511f4b1
Austin Zielinski <azielinski@csumb.edu>
2017-04-06 09:56:52 -0700

    redid changed to aws-config that were undone by a reset

--- src/aws/aws-config.js
+++ src/aws/aws-config.js
@@ -1,9 +1,14 @@
 import AWS from 'aws-sdk';
 
-const localCred = require('../../json/credentials.json');
+let access = process.env.AWS_ACCESS_KEY_ID;
+let secret = process.env.AWS_SECRET_ACCESS_KEY;
 
-const access = process.env.AWS_ACCESS_KEY_ID || localCred.accessKeyId;
-const secret = process.env.AWS_SECRET_ACCESS_KEY || localCred.secretAccessKey;
+if (access == null || secret == null) {
+  const localCred = require('../../json/credentials.json');
+  access = localCred.accessKeyId;
+  secret = localCred.secretAccessKey;
+}
+
 const dbRegion = 'us-west-1';
 const rekRegion = 'us-west-2';
 const s3Region = 'us-west-2';
