770d641f6fed036f1bcb016298ecc2137abd436b
ziel5122 <azielinski@csumb.edu>
2017-04-07 15:19:19 -0700

    edited region in various AWS configs

--- src/image/imageSupport/tagImage.js
+++ src/image/imageSupport/tagImage.js
@@ -6,7 +6,7 @@ function tagImage(file) {
     const localCredentials = require('../../../json/credentials.json');
     let  access = localCredentials.accessKeyId;
     let  secret = localCredentials.secretAccessKey;
-    let  region = localCredentials.region;
+    let  region = 'us-west-2';
 
     const credentials = new AWS.Credentials(access, secret);
 
