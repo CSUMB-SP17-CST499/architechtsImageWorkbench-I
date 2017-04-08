ab453b02f511f65f7d36881c05de8cae59086725
ziel5122 <azielinski@csumb.edu>
2017-04-08 16:15:47 -0700

    corrected names of AWS environment variables. Added node version parameter to circle.yml for future upgrades

--- src/aws/aws-config.js
+++ src/aws/aws-config.js
@@ -5,8 +5,8 @@ const dbRegion = 'us-west-1';
 const rekRegion = 'us-west-2';
 const s3Region = 'us-west-2';
 
-let access = process.env.ACCESS_KEY_ID;
-let secret = process.env.SECRET_ACCESS_KEY;
+let access = process.env.AWS_ACCESS_KEY_ID;
+let secret = process.env.AWS_SECRET_ACCESS_KEY;
 
 if (access == null || secret == null) {
   const localCredentials = require('../../json/credentials.json');
