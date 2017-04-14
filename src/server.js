55aa7fc980fbf4dfbe143710631e77dd40507aae
ziel5122 <azielinski@csumb.edu>
2017-04-14 00:42:56 -0700

    configured the app properly to be deployed and deployed it

--- src/server.js
+++ src/server.js
@@ -3,14 +3,20 @@ import path from 'path';
 import React from 'react';
 
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
 
 // ejs templates
 app.set('view engine', 'ejs');
-app.set('views', publicPath);
+app.set('views', path.join(__dirname, '..', 'public'));
 
 // define the folder that will be used for static assets
-app.use('/', express.static(publicPath));
+app.use(express.static(path.join(__dirname, '..', 'static')));
+
+app.get('/', (req, res) => {
+  let markup = '';
+  let status = 200;
+
+  return res.status(status).render('index', { markup });
+});
 
 // start the server
 const env = process.env.NODE_ENV || 'production';
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,82 @@
+var autoprefixer = require('autoprefixer');
 var path = require('path');
 var webpack = require('webpack');
 
 var APP_DIR = path.resolve(__dirname, 'src');
-var BUILD_DIR = path.resolve(__dirname, 'public');
+var BUILD_DIR = path.resolve(__dirname, 'static', 'js');
 
-var config = {
-  enrty: APP_DIR + '/index.jsx';
+module.exports = {
+  entry: APP_DIR + '/index.jsx',
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx', '']
   },
   module: {
-    rules: [
+    preLoaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'eslint',
+        include: APP_DIR
+      }
+    ],
+    loaders: [
+      {
+        exclude: [
+          /\.html$/,
+          /\.(js|jsx)(\?.*)?$/,
+          /\.css$/,
+          /\.json$/,
+          /\.svg$/
+        ],
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
+      },
       {
         test: /\.(js|jsx)$/,
         include: APP_DIR,
-        use: [
-          'babel-loader',
-          'eslint-loader'
-        ]
+        loader: 'babel',
+        query: {
+          cacheDirectory: true
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css?importLoaders=1!postcss'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.svg$/,
+        loader: 'file',
+        query: {
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
       }
+      // ** STOP ** Are you adding a new loader?
+      // Remember to add the new extension(s) to the "url" loader exclusion list.
     ]
+  },
+
+  // We use PostCSS for autoprefixing only.
+  postcss: function() {
+    return [
+      autoprefixer({
+        browsers: [
+          '>1%',
+          'last 4 versions',
+          'Firefox ESR',
+          'not ie < 9', // React doesn't support IE8 anyway
+        ]
+      }),
+    ];
   }
 };
-
-module.exports = config;
