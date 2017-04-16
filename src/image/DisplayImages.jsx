16f6d4a08e37f74d06445352e4bc6d939216e75d
Benito Sanchez <sanchezbenitog@gmail.com>
2017-04-15 22:04:33 -0700

    Expand image on image click
    Summary:
    Expands image on image click
    Enables user to click on arrow to move to next image
    Enables user to click on previus arrow to move to previous image
    
    Test Plan:
    patch on local project
    npm install
    npm start
    once all images are loaded
    click on an image
    verify that the image expands forward
    Click outside the image
    verify the image closes
    
    Click on an image again
    click on right arrow
    verify that next image is displayed
    click on left arrow
    verify that previous image is shown again
    click on 'x' at the top right of the image
    verify that the image closes back
    
    Reviewers: austinz, araceli19, gabez
    
    Differential Revision: https://imagengine.phacility.com/D39

--- src/image/DisplayImages.jsx
+++ src/image/DisplayImages.jsx
@@ -1,9 +1,79 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import Gallery from 'react-photo-gallery';
+import Lightbox from 'react-images';
 
-function DisplayImages(props) {
-  return <Gallery photos={props.imageItems} />;
+class DisplayImages extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      photos: null,
+      lightboxIsOpen: false,
+      currentImage: 0,
+      backdropClosesModal: true,
+    };
+
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.openLightbox = this.openLightbox.bind(this);
+    this.goToNext = this.goToNext.bind(this);
+    this.goToPrevious = this.goToPrevious.bind(this);
+  }
+
+  openLightbox(index, event) {
+    event.preventDefault();
+    this.setState({
+      currentImage: index,
+      lightboxIsOpen: true,
+    });
+  }
+
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    });
+  }
+
+  goToPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+
+  goToNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    });
+  }
+
+  gotoImage(index) {
+    this.setState({
+      currentImage: index,
+    });
+  }
+
+  renderGallery() {
+    return <Gallery photos={this.props.imageItems} onClickPhoto={this.openLightbox} />;
+  }
+
+  render() {
+    return (
+      <div className="DisplayImages">
+        {this.renderGallery()}
+        <Lightbox
+          images={this.props.imageItems}
+          backdropClosesModal={this.state.backdropClosesModal}
+          onClose={this.closeLightbox}
+          onClickPrev={this.goToPrevious}
+          onClickNext={this.goToNext}
+          currentImage={this.state.currentImage}
+          isOpen={this.state.lightboxIsOpen}
+          width={1600}
+        />
+      </div>
+    );
+  }
 }
 
 DisplayImages.propTypes = {
