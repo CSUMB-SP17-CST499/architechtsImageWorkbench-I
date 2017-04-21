5b8535920c33c576ac8d2149e2b4a095c3a59ad0
zapa4234 <gazapata@csumb.edu>
2017-04-21 12:43:16 -0700

    added test data to search index API
    Summary: utilized Alogolia API for searching as well as added API to package.json
    
    Test Plan: node search.js
    
    Reviewers: benFallas, araceli19, austinz
    
    Differential Revision: https://imagengine.phacility.com/D41

---
+++ src/search.js
@@ -0,0 +1,27 @@
+//create search engine via Algolia API
+
+var algoliasearch = require('algoliasearch');
+
+const loadCred = require('./json/algoliaKeys.json');
+access = loadCred.appID;
+secret = loadCred.adminKey;
+var client = algoliasearch(access, secret);
+var index = client.initIndex('img_NAME');
+
+
+
+index.saveObject({
+  firstname: 'Dog',
+  lastname: 'Cat',
+  objectID: 'myID2'
+}, function(err, content) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(content);
+});
+
+  index.search('dog', function(err, content) {
+  console.log(content.hits);
+});
