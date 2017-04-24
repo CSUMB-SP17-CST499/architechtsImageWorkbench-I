0ee384b00ff1ce7005cbeb69fe770e240cfc9b98
araceli19 <agopar@csumb.edu>
2017-04-24 00:27:08 -0700

    Displayed image tags to web app
    Summary: Image tags now diplay inside chip. User can delete unwanted tags (but can't get them back).
    
    Test Plan:
    npm install
    npm start
    upload image and wait for tags to display
    remove unwanted tags
    
    To be continued (currently working on):
    check if tags actually update on database
    work on image reload
    move chip code to another file
    
    Reviewers: benFallas, austinz
    
    Reviewed By: austinz
    
    Differential Revision: https://imagengine.phacility.com/D42

--- src/image/ImageUpload.jsx
+++ src/image/ImageUpload.jsx
@@ -1,11 +1,16 @@
 /* eslint-env browser, global FileReader */
 import React from 'react';
-
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import Chip from 'material-ui/Chip';
 import { getImages } from './support/retriever';
 import { submit } from './support/core';
 import DisplayImages from './DisplayImages';
-
+import rek from '../aws/rekcontroller';
 import './ImageUpload.css';
+
+injectTapEventPlugin();
+
+const Bucket = 'testing-uswest2';
 
 class ImageUpload extends React.Component {
   constructor(props) {
@@ -13,8 +18,17 @@ class ImageUpload extends React.Component {
     this.state = {
       imagePreviewUrl: '',
       images: [],
+      tags: [],
     };
-
+    this.styles = {
+      chip: {
+        margin: 4,
+      },
+      wrapper: {
+        display: 'flex',
+        flexWrap: 'wrap',
+      },
+    };
     this.handleImageChange = this.handleImageChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -27,12 +41,47 @@ class ImageUpload extends React.Component {
     }, true);
   }
 
+  getTags() {
+    const params = {
+      Image: {
+        S3Object: {
+          Bucket,
+          Name: this.state.file.name,
+        },
+      },
+      MinConfidence: 75,
+    };
+    const tagsImg = [];
+    return new Promise((fulfill, reject) => {
+      rek.detectLabels(params, (err, data) => {
+        if (err) reject(err);
+        fulfill(data);
+        console.log(data.Labels);
+        for (let i = 0; i < data.Labels.length; i += 1) {
+          const labelName = data.Labels[i].Name;
+          tagsImg.push({ key: i, label: labelName });
+        }
+        console.log(tagsImg);
+        this.sendTags(tagsImg);
+      });
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+     // Image Engine full process
+     // commented out location.reload() for purpose of dislaying tags
+    this.getTags();
+    submit(this.state.file, this.imgWidth, this.imgHeight, () => {
+      // location.reload();
+    });
+  }
+
   handleImageChange(e) {
     e.preventDefault();
 
     const reader = new FileReader();
     const file = e.target.files[0];
-
     reader.onloadend = () => {
       this.setState({
         file,
@@ -45,27 +94,46 @@ class ImageUpload extends React.Component {
         };
       });
     };
-
     reader.readAsDataURL(file);
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-     // Image Engine full process
-    submit(this.state.file, this.imgWidth, this.imgHeight, () => {
-      location.reload();
-    });
+  sendTags(tagsImg) {
+    this.setState({ tags: tagsImg });
   }
-
   saveImages(images) {
     this.setState({ images });
   }
+  handleRequestDelete(key) {
+    console.log(key);
+    this.tags = this.state.tags;
+    const chipToDelete = this.tags.map(chip => chip.key).indexOf(key);
+    this.tags.splice(chipToDelete, 1);
+    this.setState({ tags: this.tags });
+    // update file tags
+    submit(this.state.file, this.imgWidth, this.imgHeight, () => {
+    });
+  }
 
+  renderChip(data) {
+    console.log(data.key);
+    console.log(data.label);
+    return (
+      <Chip
+        key={data.key}
+        onRequestDelete={() => this.handleRequestDelete(data.key)}
+        style={this.styles.chip}
+      >
+        {data.label}
+      </Chip>
+    );
+  }
   render() {
     const { imagePreviewUrl } = this.state;
     const $imagePreview = imagePreviewUrl ?
+
+
       (<img className="imgUrl" id="prev" alt="imgUrl" src={imagePreviewUrl} />) :
-        'Please select an image for preview';
+       'Please select an image for preview';
 
     return (
       <div className="previewComponent">
@@ -79,6 +147,10 @@ class ImageUpload extends React.Component {
             Upload Image
           </button>
         </form>
+
+        <div className="tagPreview" style={this.styles.wrapper}>
+          {this.state.tags.map(this.renderChip, this)}
+        </div>
         <div className="imgPreview">
           {$imagePreview}
         </div>
