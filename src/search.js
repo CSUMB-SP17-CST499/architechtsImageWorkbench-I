ec333d1ffaa3e049fc296633457be380a67d705b
zapa4234 <gazapata@csumb.edu>
2017-05-02 16:35:59 -0700

    using the dynamodDB table images to get data and input in algolias index via their API.
    Summary: adding data to the img_NAME index in which will displaying in the frontend.
    
    Test Plan: npm install
    babel-node search.js
    
    Reviewers: benfallas, araceli19, austinz
    
    Subscribers:

--- src/search.js
+++ src/search.js
@@ -1,25 +1,56 @@
 //create search engine via Algolia API
 
-var algoliasearch = require('algoliasearch');
+import DBC from './aws/dbcontroller.js';
 
+var algoliasearch = require('algoliasearch');
+var _ = require('lodash');
+var async = require('async');
 const loadCred = require('./json/algoliaKeys.json');
-access = loadCred.appID;
-secret = loadCred.adminKey;
+var access = loadCred.appID;
+var secret = loadCred.adminKey;
 var client = algoliasearch(access, secret);
 var index = client.initIndex('img_NAME');
 
-index.saveObject({
-  firstname: 'Dog',
-  lastname: 'Cat',
-  objectID: 'myID3'
-}, function(err, content) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(content);
-});
+var AWS = require("aws-sdk");
 
-  index.search('dog', function(err, content) {
-  console.log(content.hits);
+ AWS.config.update({
+   accessKeyId:"AKIAIYB2L44I2CRQQQJQ",
+   secretAccessKey:"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",
+   region:"us-west-1"
+ });
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+DBC.scan({TableName: "Images"}, function (err, data) {
+  if (err) {
+             console.log (err);
+             //callback(err, null)
+         }
+          var results = JSON.stringify(data,null,1);
+
+          var obj = JSON.parse(results);
+
+          //console.log("keys of data: "+Object.keys(data['Items']));
+          //console.log("length of data: "+Object.keys(data['Items']).length)
+
+          var dataItems = data['Items'];
+          var indices = Object.keys(data['Items']);
+          var listJSON = [];
+
+          for(var i = 0; i < Object.keys(data['Items']).length; i++){
+            data['Items'][i].objectID = i;
+            //console.log(data['Items'][i].objectID);
+          }
+
+
+          indices.forEach(function(entry){
+              console.log(data['Items']);
+              index.addObject(dataItems[entry], function(err, content) {
+                if (err) {
+                  console.error(err);
+                }
+
+              });
+              listJSON.push(dataItems[entry]);
+          });
 });
