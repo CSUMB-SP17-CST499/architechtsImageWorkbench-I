6377e8a15cdca223f3da85f92612ac1ea6d8aa8a
zapa4234 <gazapata@csumb.edu>
2017-05-02 16:35:59 -0700

    deleted comments and empty space
    Summary: fixed and organized code as well as hid credentials.
    
    Test Plan: npm install
    babel-node /src/search.js
    
    Reviewers: benfallas, araceli19, austinz
    
    Subscribers:

--- src/search.js
+++ src/search.js
@@ -1,25 +1,47 @@
 //create search engine via Algolia API
 
+import DBC from './aws/dbcontroller.js';
+
 var algoliasearch = require('algoliasearch');
+var _ = require('lodash');
+var async = require('async');
 
 const loadCred = require('./json/algoliaKeys.json');
-access = loadCred.appID;
-secret = loadCred.adminKey;
+
+var access = loadCred.appID;
+var secret = loadCred.adminKey;
 var client = algoliasearch(access, secret);
 var index = client.initIndex('img_NAME');
+var AWS = require("aws-sdk");
 
-index.saveObject({
-  firstname: 'Dog',
-  lastname: 'Cat',
-  objectID: 'myID3'
-}, function(err, content) {
+ AWS.config.update({
+   region:"us-west-1"
+ });
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+DBC.scan({TableName: "Images"}, function (err, data) {
   if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(content);
-});
+             console.log (err);
+             //callback(err, null)
+         }
+          var results = JSON.stringify(data,null,1);
+          var obj = JSON.parse(results);
+          var dataItems = data['Items'];
+          var indices = Object.keys(data['Items']);
+          var listJSON = [];
 
-  index.search('dog', function(err, content) {
-  console.log(content.hits);
+          for(var i = 0; i < Object.keys(data['Items']).length; i++){
+            data['Items'][i].objectID = i;
+          }
+
+          indices.forEach(function(entry){
+              console.log(data['Items']);
+              index.addObject(dataItems[entry], function(err, content) {
+                if (err) {
+                  console.error(err);
+                }
+              });
+              listJSON.push(dataItems[entry]);
+          });
 });
