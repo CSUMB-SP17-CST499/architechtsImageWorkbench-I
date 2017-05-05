ca94746adcfb28451b54f7f92cba57b5f1fcec01
zapa4234 <gazapata@csumb.edu>
2017-05-05 12:41:04 -0700

    created backend for search with algolia to import images
    Summary: backend for search via algolia
    
    Test Plan:
    npm install
    babel-node search.js
    log into algolia
    go to indices
    search for imported image by tag(s)
    
    Reviewers: araceli19, benFallas, austinz
    
    Differential Revision: https://imagengine.phacility.com/D47

--- src/aws/aws-config.js
+++ src/aws/aws-config.js
@@ -9,7 +9,7 @@ let access = process.env.AWS_ACCESS_KEY_ID;
 let secret = process.env.AWS_SECRET_ACCESS_KEY;
 
 if (access == null || secret == null) {
-  const localCredentials = require('../../json/credentials.json');
+  const localCredentials = require('../json/credentials.json');
   access = localCredentials.accessKeyId;
   secret = localCredentials.secretAccessKey;
 }
